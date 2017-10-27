8a53dbff4a649902ac5f58b60e7fd4cdda30323d
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2017-10-26 19:07:34 -0700

    frontend commit

---
+++ frontend/src/app/messages.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'messages',
+    template: `
+        <div *ngFor="let message of messages">
+            <mat-card style="margin:8px">
+                <mat-card-title>{{message.owner}}</mat-card-title>
+                <mat-card-content>{{message.text}}</mat-card-content>
+            </mat-card>
+        </div>
+        <button md-button>test</button>
+    `
+})
+export class MessagesComponent {
+    messages = [{text:'some text', owner:'Tim'}, {text:'other message', owner:'Jane'}];
+}
