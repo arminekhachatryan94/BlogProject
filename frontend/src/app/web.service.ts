364cc05cd9c8beb6c02a428c590a5cf8ee80a814
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2017-10-28 15:04:15 -0700

    owner filter

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -10,16 +10,16 @@ export class WebService {
     messages = [];
 
     constructor(private http: Http, private sb: MatSnackBar) {
-        this.getMessages();
+        // this.getMessages();
     }
 
-    async getMessages() {
-        try {
-            var response = await this.http.get(this.BASE_URL + '/messages').toPromise();
-            this.messages = response.json();    
-        } catch (error) {
+    getMessages(user) {
+        user = (user) ? '/' + user : '';
+        this.http.get(this.BASE_URL + '/messages' + user).subscribe(response => {
+            this.messages = response.json();
+        }, error => {
             this.handleError("Unable to get messages");
-        }
+        });
     }
 
     async postMessage(message) {
