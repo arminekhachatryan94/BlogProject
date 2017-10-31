c4bcafd4fe7ba2658f0738617230c69a0c3df24c
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2017-10-30 22:53:39 -0700

    auth & register service

---
+++ backend/backend/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace backend.Controllers
+{
+    public class JwtPacket
+    {
+        public string Token { get; set; }
+    }
+
+    [Produces("application/json")]
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        
+        [HttpPost("register")]
+        public JwtPacket Register([FromBody]Models.User user) {
+            var jwt = new JwtSecurityToken();
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return new JwtPacket() { Token = encodedJwt };
+
+        }
+    }
+}
