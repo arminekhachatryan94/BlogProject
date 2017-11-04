372f6a9d6bd1af00eb0edd6bb7917739971299d1
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2017-11-03 22:00:51 -0700

    get user info

---
+++ backend/backend/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+
+        readonly ApiContext context;
+
+        public UsersController(ApiContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(string id) {
+            var user = context.users.SingleOrDefault(u => u.Id == id);
+
+            if (user == null)
+                return NotFound("User not found");
+
+            return Ok(user);
+        }
+    }
+}
